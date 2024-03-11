fdc890e63170db28cf8970f1c25f19b92afdc628
Rosen Popov <50305462+rdpopov@users.noreply.github.com>
2024-03-11 02:32:15 +0200

    Update setup_dots.sh

--- setup_dots.sh
+++ setup_dots.sh
@@ -2,4 +2,6 @@
 
 find -iname dotfiles_setup.sh|xargs -n 1 sh
 find -iname setup.sh|xargs -n 1 sh
-echo $(find -iname DEPS |xargs cat |sort|uniq|sed 's/\\n//g')
+$(cd $HOME/.config/nvim && git pull origin master)
+$(cd $HOME/.config/quoty && git pull origin master)
+find -iname DEPS |xargs cat |sort|uniq|sed 's/\\n//g' | sudo xargs apt install -y
